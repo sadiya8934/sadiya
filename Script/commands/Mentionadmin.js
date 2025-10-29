70e076b9ddcae9b061f9080bad7a9414730d2b8a
sadiya8934 <csadiya797@gmail.com>
2025-10-29 14:13:59 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61575698041722", "100001039692046", "61576394315037"].map(String);
+ const adminIDs = ["61579480017209", "100066319245507", "100084496188722"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
